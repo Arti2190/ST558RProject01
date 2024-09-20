ccd865d3bbb39bbd3f4b1fd79dac3ea32ea67ed4
Gupta <rsgupta2024@gmail.com>
2024-09-19 22:36:22 -0400

    Check code commit

---
+++ Main.R
@@ -0,0 +1,3 @@
+#API key - d116b830d71b6bc7b93f5ad62fbce8f95e8a5b59
+#API -   https://api.census.gov/data/2022/acs/acs1/pums?get=SEX,PWGTP,MAR&SCHL=24&key=YOUR_KEY
+#https://api.census.gov/data/2022/acs/acs1/pums?get=SEX,PWGTP,MAR&SCHL=24&key=d116b830d71b6bc7b93f5ad62fbce8f95e8a5b59
